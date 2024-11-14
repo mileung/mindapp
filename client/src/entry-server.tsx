0590ebeaa44bb6329d0e39fbe635552d089f5e7b
mileung <mileung@proton.me>
2024-11-14 00:07:25 -0800

    open graph

--- client/src/entry-server.tsx
+++ client/src/entry-server.tsx
@@ -15,9 +15,23 @@ export default createHandler(() => {
 						<head>
 							<meta charset="utf-8" />
 							<meta name="viewport" content="width=device-width, initial-scale=1" />
+							<title>Mindapp</title>
+							<meta name="description" content="cc = collective consciousness" />
+
+							<link rel="manifest" href="/manifest.json" />
 							<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 							<link rel="icon" href="/mindapp-logo.svg" />
-							<link rel="manifest" href="/manifest.json" />
+							<meta property="og:url" content="https://mindapp.cc/" />
+							<meta property="og:type" content="website" />
+							<meta property="og:title" content="Mindapp" />
+							<meta property="og:description" content="cc = collective consciousness" />
+							<meta property="og:image" content="https://mindapp.cc/mindapp-og-bg.png" />
+							<meta name="twitter:card" content="summary_large_image" />
+							<meta property="twitter:domain" content="mindapp.cc" />
+							<meta property="twitter:url" content="https://mindapp.cc/" />
+							<meta name="twitter:title" content="Mindapp" />
+							<meta name="twitter:description" content="cc = collective consciousness" />
+							<meta name="twitter:image" content="https://mindapp.cc/mindapp-og-bg.png" />
 							{assets}
 						</head>
 						<body>
