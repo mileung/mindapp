3b251772a8a7607c41e40002092f8cf319d15b65
mileung <mileung@proton.me>
2026-09-08 12:23:02 -0700

    20 worked, 100?

--- src/lib/types/posts/getPostFeed.ts
+++ src/lib/types/posts/getPostFeed.ts
@@ -454,7 +454,7 @@ export let _getPostFeed = async (
 							),
 						];
 						if (sectionHasRequiredTags) {
-							let maxOrLeaves = 20;
+							let maxOrLeaves = 100;
 							let chunkSize = Math.max(
 								1,
 								Math.floor(maxOrLeaves / _tag_imBy8_countEitherRows.length),
