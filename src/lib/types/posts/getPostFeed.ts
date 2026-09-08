419314f23d4b874bba2d4eff4b2c7c7656aeeae0
mileung <mileung@proton.me>
2026-09-08 12:19:45 -0700

    hm.

--- src/lib/types/posts/getPostFeed.ts
+++ src/lib/types/posts/getPostFeed.ts
@@ -454,7 +454,7 @@ export let _getPostFeed = async (
 							),
 						];
 						if (sectionHasRequiredTags) {
-							let maxOrLeaves = 200;
+							let maxOrLeaves = 20;
 							let chunkSize = Math.max(
 								1,
 								Math.floor(maxOrLeaves / _tag_imBy8_countEitherRows.length),
